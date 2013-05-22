2fa4150b17de3e5d84f28ba08b5a932793a59762
Shou Ya <shouyatf@gmail.com>
2013-05-23 00:16:03 +0800

    solved problem 19

--- 19.hs
+++ 19.hs
@@ -3,5 +3,16 @@
 rotate :: [a] -> Int -> [a]
 rotate xs 0 = xs
 rotate xs n
-  | n < 0 = (last xs) : rotate (init xs) (n + 1)
-  | n > 0 = rotate (tail xs) (n - 1) ++ [head xs]
+  | n < 0 = rotate (last xs : init xs) (n + 1)
+  | n > 0 = rotate (tail xs ++ [head xs]) (n - 1)
+
+
+--
+-- n = -2
+-- xs = "abc"
+
+-- n < 0 --> ab c
+-- n < 0 --> ca b
+-- n < 0 --> bc a
+
+--
